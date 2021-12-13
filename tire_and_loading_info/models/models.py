eb1a703c709679ffbe47c19eb1e525e0ed518eb3
edtrailersmfg <jacob@edzinktrailer.com>
2021-12-13 13:45:43 -0700

    Update models.py

--- tire_and_loading_info/models/models.py
+++ tire_and_loading_info/models/models.py
@@ -5,12 +5,12 @@ from odoo.tools import float_is_zero
 
 class InfoReporte(models.AbstractModel):
     
-    _name = "studio_customization.x_tire_loading_info_card"
+    _name = "x_tire_loading_info.x_tire_loading_info_card"
 
     @api.model
     def _get_report_values(self, docids, data=None):
         report_obj = self.env['ir.actions.report']
-        report = report_obj._get_report_from_name('studio_customization.x_tire_loading_info_card')
+        report = report_obj._get_report_from_name('x_tire_loading_info.x_tire_loading_info_card')
         return{
             'doc_ids': docids,
             'doc_model': self.env['studio_customization.x_tire_loading_info'],
