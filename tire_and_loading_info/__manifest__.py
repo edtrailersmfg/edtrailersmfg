e261c9bcc7d57397cc569fc5bc9107f18822c5c5
edtrailersmfg <jacob@edzinktrailer.com>
2021-12-13 12:46:26 -0700

    tire and loading

---
+++ tire_and_loading_info/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Tire and Loading Info',
+    'version': '15.0.1.0.1',
+    'category': 'Inventory',
+    'summary': "Print format for tire and loading info'",
+    'author': 'Sursoom',
+    'maintainer': 'Sursoom',
+    'company': 'Sursoom',
+    'website': 'https://www.sursoom.mx',
+    'description': """Imprime reporte del formato'""",
+    'depends': ['stock',
+                'stock_account',
+                'purchase',
+                'base'
+                ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
