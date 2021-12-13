1eddf7f9ce3b88bd5dea043eb5a9db00a09ad1f5
edtrailersmfg <jacob@edzinktrailer.com>
2021-12-13 13:49:51 -0700

    Update models.py

--- tire_and_loading_info/models/models.py
+++ tire_and_loading_info/models/models.py
@@ -7,14 +7,14 @@ class InfoReporte(models.AbstractModel):
     
     _name = "x_tire_loading_info.x_tire_loading_info_card"
 
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        report_obj = self.env['ir.actions.report']
-        report = report_obj._get_report_from_name('x_tire_loading_info.x_tire_loading_info_card')
-        return{
-            'doc_ids': docids,
-            'doc_model': self.env['studio_customization.x_tire_loading_info'],
-            'docs': self.env['studio_customization.x_tire_loading_info'].browse(docids)
-        }
-
+#
+#    @api.model
+#    def _get_report_values(self, docids, data=None):
+#        report_obj = self.env['ir.actions.report']
+#        report = report_obj._get_report_from_name('x_tire_loading_info.x_tire_loading_info_card')
+#        return{
+#            'doc_ids': docids,
+#            'doc_model': self.env['studio_customization.x_tire_loading_info'],
+#            'docs': self.env['studio_customization.x_tire_loading_info'].browse(docids)
+#        }
 
