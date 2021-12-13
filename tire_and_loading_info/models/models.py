ef8027e8c3ec2b4df43aaee6d481b9b6934bdd72
edtrailersmfg <jacob@edzinktrailer.com>
2021-12-13 13:59:31 -0700

    data

--- tire_and_loading_info/models/models.py
+++ tire_and_loading_info/models/models.py
@@ -4,9 +4,7 @@ from odoo.exceptions import UserError
 from odoo.tools import float_is_zero
 
 class InfoReporte(models.AbstractModel):
-
-    _inherit = "studio_customization.tire_and_loading_info"
-    
+    _inherit = "studio_customization.tire_and_loading_info" 
     _name = "x_tire_loading_info.x_tire_loading_info_card"
 
     @api.model
