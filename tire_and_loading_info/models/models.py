706a52a3e9dd6e763db4bc69c48604557631c1a6
edtrailersmfg <jacob@edzinktrailer.com>
2021-12-13 13:55:19 -0700

    Update models.py

--- tire_and_loading_info/models/models.py
+++ tire_and_loading_info/models/models.py
@@ -4,17 +4,17 @@ from odoo.exceptions import UserError
 from odoo.tools import float_is_zero
 
 class InfoReporte(models.AbstractModel):
+
+    _inherit = "studio_customization.tire_and_loading_info"
     
     _name = "x_tire_loading_info.x_tire_loading_info_card"
 
-#
-#    @api.model
-#    def _get_report_values(self, docids, data=None):
-#        report_obj = self.env['ir.actions.report']
-#        report = report_obj._get_report_from_name('x_tire_loading_info.x_tire_loading_info_card')
-#        return{
-#            'doc_ids': docids,
-#            'doc_model': self.env['studio_customization.x_tire_loading_info'],
-#            'docs': self.env['studio_customization.x_tire_loading_info'].browse(docids)
-#        }
-
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        report_obj = self.env['ir.actions.report']
+        report = report_obj._get_report_from_name('x_tire_loading_info.x_tire_loading_info_card')
+        return{
+            'doc_ids': docids,
+            'doc_model': self.env['studio_customization.x_tire_loading_info'],
+            'docs': self.env['studio_customization.x_tire_loading_info'].browse(docids)
+        }
