87ce7420db029af53da5dd01b2cd504c5da22857
edtrailersmfg <jacob@edzinktrailer.com>
2022-03-01 18:24:37 -0700

    Update delivery orders

--- logistics_webapp/controllers/delivery_orders.py
+++ logistics_webapp/controllers/delivery_orders.py
@@ -5,15 +5,11 @@ class web_delivery_orders(http.Controller):
 
     @http.route('/delivery-orders-query/load',type="http",website=True,auth='public')
     def estatus_main_seguimiento(self,**kw):
-        cliente=request.env['res.partner'].sudo().search([('email','=',request.session.login)])
-        if cliente:
-            lista_datos=request.env['logistica.ordenes_venta_carga'].sudo().search([('transportista','=',cliente.id)])
-            print(lista_datos)
-            return http.request.render('logistics_webapp.delivery-order-query',{
-                'lista_datos':lista_datos,
-                })
-
-
-
-
-
+        if request.session.login:
+            cliente=request.env['res.partner'].sudo().search([('email','=',request.session.login)])
+            if cliente:
+                lista_datos=request.env['logistica.ordenes_venta_carga'].sudo().search([('transportista','=',cliente.id)])
+                print(lista_datos)
+                return http.request.render('logistics_webapp.delivery-order-query',{
+                    'lista_datos':lista_datos,
+                    })
