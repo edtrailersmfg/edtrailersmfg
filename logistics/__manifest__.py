91a07f656c25bf64a17c7722a5ad7d8bd1c06b03
Enrique Jaquez <92959031+edtrailersmfg@users.noreply.github.com>
2022-04-07 09:21:05 -0600

    Update Logistics

--- logistics/__manifest__.py
+++ logistics/__manifest__.py
@@ -11,7 +11,7 @@
     """,
 
     'author': "Sursoom",
-    'website': "http://www.sursom.mx",
+    'website': "https://www.sursom.mx",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
