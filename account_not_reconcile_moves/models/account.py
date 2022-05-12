862726db708717871b6f96508f9947899d78b777
Germán Ponce Dominguez <german.ponce@argil.mx>
2022-05-12 10:51:03 -0500

    Actualizaciones

--- account_not_reconcile_moves/models/account.py
+++ account_not_reconcile_moves/models/account.py
@@ -49,45 +49,6 @@ class AccountMoveLine(models.Model):
         res = super(AccountMoveLine, self)._create_exchange_difference_move()
         return res
 
-class AccountMoveReversal(models.TransientModel):
-    _inherit = 'account.move.reversal'
-
-    def _prepare_default_reversal(self, move):
-        res = super(AccountMoveReversal, self)._prepare_default_reversal(move=move)
-        move.invoice_reversed = True
-        res.update({
-                        'invoice_origin_reverse_id': move.id,
-                    })
-        return res
-
-class AccountPartialReconcile(models.Model):
-    _inherit = "account.partial.reconcile"
-
-    def _create_tax_cash_basis_moves(self):
-        for partial in self:
-            debit_move_type = partial.debit_move_id.move_id.move_type
-            credit_move_type = partial.credit_move_id.move_id.move_type
-            if debit_move_type == 'out_invoice' and credit_move_type == 'out_refund':
-                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
-                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
-                return False
-            elif credit_move_type == 'out_invoice' and debit_move_type == 'out_refund':
-                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
-                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
-                return False
-            elif debit_move_type == 'in_invoice' and credit_move_type == 'in_refund':
-                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
-                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
-                return False
-            elif credit_move_type == 'in_invoice' and debit_move_type == 'in_refund':
-                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
-                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
-                return False
-        res = super(AccountPartialReconcile, self)._create_tax_cash_basis_moves()
-        return res
-
-class AccountMoveLine(models.Model):
-    _inherit ='account.move.line'
 
     def reconcile(self):
         ''' Reconcile the current move lines all together.
@@ -201,3 +162,41 @@ class AccountMoveLine(models.Model):
             .action_invoice_paid()
 
         return results
+
+
+class AccountMoveReversal(models.TransientModel):
+    _inherit = 'account.move.reversal'
+
+    def _prepare_default_reversal(self, move):
+        res = super(AccountMoveReversal, self)._prepare_default_reversal(move=move)
+        move.invoice_reversed = True
+        res.update({
+                        'invoice_origin_reverse_id': move.id,
+                    })
+        return res
+
+class AccountPartialReconcile(models.Model):
+    _inherit = "account.partial.reconcile"
+
+    def _create_tax_cash_basis_moves(self):
+        for partial in self:
+            debit_move_type = partial.debit_move_id.move_id.move_type
+            credit_move_type = partial.credit_move_id.move_id.move_type
+            if debit_move_type == 'out_invoice' and credit_move_type == 'out_refund':
+                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
+                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
+                return False
+            elif credit_move_type == 'out_invoice' and debit_move_type == 'out_refund':
+                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
+                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
+                return False
+            elif debit_move_type == 'in_invoice' and credit_move_type == 'in_refund':
+                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
+                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
+                return False
+            elif credit_move_type == 'in_invoice' and debit_move_type == 'in_refund':
+                _logger.info("\n######### Es una conciliación de factura contra nota de credito >>>>>>>>>>>> ")
+                _logger.info("\n######### No creamos partida de reclasificación de Impuestos >>>>>>>>>>>> ")
+                return False
+        res = super(AccountPartialReconcile, self)._create_tax_cash_basis_moves()
+        return res
