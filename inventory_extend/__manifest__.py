e0daf73e45c2ae18e9e160158ea4adc2023358dc
Enrique Jaquez Olvera <enrique.jaquez@sursoom.mx>
2023-01-16 15:24:15 -0600

    update inventory_extend

--- inventory_extend/__manifest__.py
+++ inventory_extend/__manifest__.py
@@ -8,7 +8,7 @@ Tipo de Cambio para Calcular Costo y Precio del Producto
     """,
     'version' : '15.0.2',
     'category': 'Inventory/Inventory',
-    'author': 'Enrique Jaquez',
+    'author': 'Enrique J',
     'depends' : [
         'account',
         'base',
