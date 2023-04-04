e81cda6b64359297d6fb6a5af1ddf70da61686d9
Enrique Jaquez <92959031+edtrailersmfg@users.noreply.github.com>
2023-04-04 17:09:45 -0600

    update accounting update amount usd

--- accounting_update_amount_usd/models/models.py
+++ accounting_update_amount_usd/models/models.py
@@ -69,24 +69,21 @@ class RegisterPayments(models.Model):
 		tc = location_model.search([("currency_id", "=", "USD"),("name","=",current_date)])
 		self.tipo_cambio = tc.inverse_company_rate
 		self.fecha_tc = tc.create_date
-		self.x_studio_tc = tc.inverse_company_rate
+		#self.x_studio_tc = tc.inverse_company_rate
 		#raise UserError("tipo de cambio " + str(tc.inverse_company_rate))
 
 	def _compute_importe_usd(self):
 		for record in self:
 			if len(self) == 1:
 				if record.tipo_cambio > 0:
-					record.importe_usd = record.amount_total_signed / record.tipo_cambio
-					if record.currency_id.name == "USD":
-						record.amount_total_in_currency_signed = record.importe_usd
+					record.importe_usd = record.amount / record.tipo_cambio
 
 	def _compute_importe_mxn(self):
 		for record in self:
 			if len(self) == 1:
 				if record.tipo_cambio > 0:
-					record.importe_mxn = record.amount_total_signed * ( record.tipo_cambio / record.tipo_cambio )
-					if record.currency_id.name == "MXN":
-						record.amount_total_in_currency_signed = record.importe_mxn
+					record.importe_mxn = record.amount * ( record.tipo_cambio / record.tipo_cambio )
+
 
 
 
