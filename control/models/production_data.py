2dfe9430ec0f05f35eff824d9e0739745f71c3bf
edtrailersmfg <jacob@edzinktrailer.com>
2023-06-07 17:35:46 -0700

    update control app

--- control/models/production_data.py
+++ control/models/production_data.py
@@ -21,12 +21,21 @@ class Production_Data(models.Model):
         )
     terminado = fields.Boolean(string='Terminado')
     hold = fields.Boolean(string='Hold')
+    semana_laboral = fields.Selection([
+            ('1','1'),('2','2'),('3','3'),('4','4'),('5','5'),('6','6'),('7','7'),('8','8'),('9','9'),('10','10'),
+            ('11','11'),('12','12'),('13','13'),('14','14'),('15','15'),('16','16'),('17','17'),('18','18'),('19','19'),('20','20'),
+            ('21','21'),('22','22'),('23','23'),('24','24'),('25','25'),('26','26'),('27','27'),('28','28'),('29','29'),('30','30'),
+            ('31','31'),('32','32'),('33','33'),('34','34'),('35','35'),('36','36'),('37','37'),('38','38'),('39','39'),('40','40'),
+            ('41','41'),('42','42'),('43','43'),('44','44'),('45','45'),('46','46'),('47','47'),('48','48'),('49','49'),('50','50'),
+            ('51','51'),('52','52')], string="Semana Laboral"
+        )
 
     @api.onchange('centro_trabajo')
     def on_change_centro_trabajo(self):
         for record in self:
             if record.centro_trabajo == 'Terminado':
                 record.terminado = True
+                record.hold = False
             elif record.centro_trabajo == 'Hold':
                 record.terminado = False
                 record.hold = True
