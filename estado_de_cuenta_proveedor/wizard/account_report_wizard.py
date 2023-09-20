b4bfff3a9b772f51216d3eafa9f8dfe7b0e367b5
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-20 15:45:39 -0700

    update estado_de_cuenta_proveedor

--- estado_de_cuenta_proveedor/wizard/account_report_wizard.py
+++ estado_de_cuenta_proveedor/wizard/account_report_wizard.py
@@ -8,7 +8,7 @@ class AccounutReportWizard(models.TransientModel):
     _name = "account.report.wizard"
     _description = "Estado de Cuenta del Proveedor"
 
-    supplier = fields.Many2one('res.partner', string="Proveedor : ")
+    supplier = fields.Many2one('res.partner', string="Proveedor", domain="[('supplier_rank', '>', 0)]")
     start_date = fields.Datetime(string="Fecha Inicial : ", default=lambda self: fields.datetime.now())
     end_date = fields.Datetime(string="Fecha Final : ", default=lambda self: fields.datetime.now())
 
