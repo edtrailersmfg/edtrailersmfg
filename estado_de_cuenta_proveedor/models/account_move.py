04f62c19c6e8021fca481e385ba58dbb3311ce04
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-22 11:50:59 -0700

    update estado_de_cuenta_proveedor
    update estado_de_cuenta_proveedor

--- estado_de_cuenta_proveedor/models/account_move.py
+++ estado_de_cuenta_proveedor/models/account_move.py
@@ -5,10 +5,16 @@ from datetime import datetime
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    dias = fields.Char(string="Días Vencidos", compute="_compute_dias", store=True)
+    dias = fields.Integer(string="Días Vencidos", compute="_compute_dias", store=True)
 
     @api.depends('invoice_date', 'invoice_date_due')
     def _compute_dias(self):
+        fmt = '%Y-%m-%d'
         for rec in self:
-            rec.dias = (rec.invoice_date_due - rec.invoice_date).days
+            rec.dias = 0
+            start_date = rec.invoice_date
+            end_date = rec.invoice_date_due
+            d1 = datetime.strptime(start_date, fmt)
+            d2 = datetime.strptime(end_date, fmt)
+            rec.dias = (d2 - d1).days
 
