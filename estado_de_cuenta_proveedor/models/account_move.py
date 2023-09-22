f3d50c68242ed1ef0062527e0bf64eb8b20ed001
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-22 14:43:02 -0700

    update estado_de_cuenta_proveedor
    update estado_de_cuenta_proveedor

--- estado_de_cuenta_proveedor/models/account_move.py
+++ estado_de_cuenta_proveedor/models/account_move.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api, _
-from datetime import datetime
+from datetime import datetime, date
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     dias = fields.Char(string="Días Vencidos", compute="_compute_dias", store=True)
 
-    @api.depends('invoice_date', 'invoice_date_due')
+    @api.depends('invoice_date_due','invoice_date')
     def _compute_dias(self):
-        now = datetime.now()
         for rec in self:
-            f1_str = now.strftime('%d/%m/%Y')
-            rec.dias = f1_str
+            if rec.invoice_date_due:
+                rec.dias = (rec.invoice_date_due - rec.invoice_date).days
+            else
+                rec.dias = 0
 
