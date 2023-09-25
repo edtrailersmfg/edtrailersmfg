e0b2d7b119130e2ebb79cdf23a8fef21e7d97bc8
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-25 14:21:07 -0700

    update estado_de_cuenta_proveedor
    update estado_de_cuenta_proveedor

--- estado_de_cuenta_proveedor/models/account_move.py
+++ estado_de_cuenta_proveedor/models/account_move.py
@@ -17,7 +17,8 @@ class AccountMove(models.Model):
                 #fecha_limite = datetime.strptime(rec.invoice_date_due, "%Y-%m-%d")
                 fecha_limite = rec.invoice_date_due
                 tipo_fecha_limite = str(type(fecha_limite))
-                raise UserError("Fecha límite %s - Fecha actual %s" %tipo_fecha_limite %tipo_fecha_actual)
+                #raise UserError("Fecha límite %s - Fecha actual %s" %tipo_fecha_limite %tipo_fecha_actual)
+                raise UserError("Fecha límite %s" %tipo_fecha_limite)
                 #diferencia = fecha_actual - fecha_limite
                 #diferencia_en_dias = diferencia.days
                 #rec.dias = diferencia_en_dias
