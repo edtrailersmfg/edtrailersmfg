8afc9a7a48652059adb19f41eef5da0227ba430e
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-25 14:49:58 -0700

    update estado_de_cuenta_proveedor
    update estado_de_cuenta_proveedor

--- estado_de_cuenta_proveedor/models/account_move.py
+++ estado_de_cuenta_proveedor/models/account_move.py
@@ -11,12 +11,13 @@ class AccountMove(models.Model):
     @api.depends('invoice_date_due','invoice_date')
     def _compute_dias(self):
         fecha_actual = datetime.now().strftime('%Y-%m-%d')
+        fecha_dt = datetime.strptime(fecha_actual, '%Y-%m-%d')
         for rec in self:
             if rec.invoice_date_due:
                 #fecha_limite = datetime.strptime(rec.invoice_date_due, "%Y-%m-%d")
                 fecha_limite = rec.invoice_date_due
                 #raise UserError("Fecha límite %s" %tipo_fecha_limite)
-                diferencia = fecha_actual - fecha_limite
+                diferencia = fecha_dt - fecha_limite
                 raise UserError("diferencia %s" %diferencia)
                 #diferencia_en_dias = diferencia.days
                 #rec.dias = diferencia_en_dias
