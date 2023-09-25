c0d6637a6ea63f15ebb87b3e11b63afadd2253b6
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-09-25 16:27:05 -0700

    install estado_de_cuenta_proveedor app
    install estado_de_cuenta_proveedor app

---
+++ estado_de_cuenta_proveedor/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Estado de Cuenta del Proveedor",
+    "summary": """
+        Estado de Cuenta del Proveedor
+        Reporte de Estado de Cuenta del Proveedor
+        Muestra el listado de Facturas Sin Pago y con Pago Parcial
+    """,
+    "version": "15.0.1",
+    "category": "Account",
+    "website": "https://www.sursoom.mx",
+    "author": "Enrique Jaquez",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'base',
+        'account',
+        'report_xlsx'
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'wizard/account_report_wizard_views.xml',
+        'report/account_report_template_views.xml',
+        'report/account_report.xml',
+        'views/account_move_views.xml'
+    ],
+}
