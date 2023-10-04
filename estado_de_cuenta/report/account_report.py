80628431a970f3eef03ed1d8b6732667a7c720c7
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-10-04 09:56:34 -0700

    update estado de cuenta

--- estado_de_cuenta/report/account_report.py
+++ estado_de_cuenta/report/account_report.py
@@ -10,11 +10,9 @@ class ReportInvoiceWithPayment(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['account.report.wizard'].browse(docids)
         move_data = docs._get_move_data_report_values()
-        move_data2 = docs._get_move_data_report_values2()
         return {
             'doc_ids': docids,
             'doc_model': 'account.report.wizard',
             'docs': docs,
             'move_data': move_data,
-            'move_data2': move_data2,
         }
