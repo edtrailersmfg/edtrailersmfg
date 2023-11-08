a8312b8e939b428f9f50289664a0702f4ca96a64
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-11-08 11:51:34 -0600

    update warehouse_extend
    update warehouse_extend

--- warehouse_extend/models/product.py
+++ warehouse_extend/models/product.py
@@ -10,45 +10,19 @@ class ProductTemplate(models.Model):
 
     def _get_warehouse_quantity(self):
         for record in self:
-            warehouse_quantity = 0
+            x_warehouse01_quantity = 0
+            x_warehouse02_quantity = 0
             product_id = self.env['product.product'].sudo().search([('product_tmpl_id', '=', record.id)])
             if product_id:
                 quant_ids = self.env['stock.quant'].sudo().search([('product_id','=',product_id[0].id),('location_id.usage','=','internal')])
-                t_warehouses = {}
+                #t_warehouses = {}
                 for quant in quant_ids:
                     if quant.location_id:
-                        if quant.location_id not in t_warehouses:
-                            t_warehouses.update({quant.location_id:0})
-                        t_warehouses[quant.location_id] += quant.quantity
+                        if quant.location_id.name == "ALMACEN DE MATERIALES"
+                            x_warehouse01_quantity = quant.quantity
+                        if quant.location_id.name == "ALMACEN EN PROCESO"
+                            x_warehouse02_quantity = quant.quantity
+                    record.warehouse01_quantity = x_warehouse01_quantity
+                    record.warehouse02_quantity = x_warehouse02_quantity
 
-                tt_warehouses = {}
-                for location in t_warehouses:
-                    warehouse = False
-                    location1 = location
-                    while (not warehouse and location1):
-                        warehouse_id = self.env['stock.warehouse'].sudo().search([('lot_stock_id','=',location1.id)])
-                        if len(warehouse_id) > 0:
-                            warehouse = True
-                        else:
-                            warehouse = False
-                        location1 = location1.location_id
-                    if warehouse_id:
-                        if warehouse_id.name not in tt_warehouses:
-                            tt_warehouses.update({warehouse_id.name:0})
-                        tt_warehouses[warehouse_id.name] += t_warehouses[location]
-
-                for item in tt_warehouses:
-                    #if tt_warehouses[item] != 0:
-                    if (item == "ALMACEN DE MATERIALES"):
-                        if tt_warehouses[item]:
-                            warehouse_quantity = tt_warehouses[item]
-                            record.warehouse01_quantity = warehouse_quantity
-                        else:
-                            record.warehouse01_quantity = 0
-                    if (item == "ALMACEN EN PROCESO"):
-                        if tt_warehouses[item]:
-                            warehouse_quantity = tt_warehouses[item]
-                            record.warehouse02_quantity = warehouse_quantity
-                        else:
-                            record.warehouse02_quantity = 0
 
