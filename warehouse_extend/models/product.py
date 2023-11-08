784c73264b7cbc05e9efd87dd5755793466a4e43
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-11-08 12:42:13 -0600

    update warehouse_extend
    update warehouse_extend

--- warehouse_extend/models/product.py
+++ warehouse_extend/models/product.py
@@ -23,7 +23,7 @@ class ProductTemplate(models.Model):
                         if quant.location_id.name == "StockMP":
                             x_warehouse01_quantity = x_warehouse01_quantity + quant.quantity
                         if quant.location_id.name == "StockPRO":
-                            x_warehouse02_quantity = x_warehouse01_quantity + quant.quantity
+                            x_warehouse02_quantity = x_warehouse02_quantity + quant.quantity
                 record.warehouse01_quantity = x_warehouse01_quantity
                 record.warehouse02_quantity = x_warehouse02_quantity
 
