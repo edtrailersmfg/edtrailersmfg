97e74ac6012d72666f1a4ba4b533a0593bdf4ac0
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-11-08 10:44:48 -0600

    install warehouse_extend
    install warehouse_extend

---
+++ warehouse_extend/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Cantidad de Producto en Almacén",
+    'author': 'Enrique Jaquez',
+    'category': 'Warehouse',
+    'summary': """
+                    Muestra los productos disponibles por almacén, lo muestra en la vista lista y vista formulario en el producto.
+                    Se crean 2 variables en el modelo product.template:
+                      - 
+                """,
+    'license': 'AGPL-3',
+    'website': '',
+    'description': """
+""",
+    'version': '15.0.1',
+    'depends': ['sale','stock'],
+    'data': ['views/product_view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
