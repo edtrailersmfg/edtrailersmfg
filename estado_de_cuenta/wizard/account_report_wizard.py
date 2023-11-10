0d3191369601fde04415aa6d082d00be4d607f44
Enrique Jaquez <90928324+enjaquez@users.noreply.github.com>
2023-11-10 11:17:52 -0600

    update estado_de_cuenta
    update estado_de_cuenta

--- estado_de_cuenta/wizard/account_report_wizard.py
+++ estado_de_cuenta/wizard/account_report_wizard.py
@@ -24,10 +24,9 @@ class AccounutReportWizard(models.TransientModel):
         domain = [
             ('state', '=', 'posted'),
             ('move_type', '=', 'out_invoice'),
-            ('payment_state', '!=', 'paid'),
-            ('payment_state', '!=', 'in_payment'),
-            ('payment_state', '!=', 'reversed'),
             ('currency_id', '=', 'USD'),
+            ('invoice_date', '>=', self.start_date),
+            ('invoice_date', '<=', self.end_date),            
         ]
         if docs.customer:
             domain.append(
@@ -42,10 +41,9 @@ class AccounutReportWizard(models.TransientModel):
         domain = [
             ('state', '=', 'posted'),
             ('move_type', '=', 'out_invoice'),
-            ('payment_state', '!=', 'paid'),
-            ('payment_state', '!=', 'in_payment'),
-            ('payment_state', '!=', 'reversed'),
             ('currency_id', '=', 'MXN'),
+            ('invoice_date', '>=', self.start_date),
+            ('invoice_date', '<=', self.end_date),            
         ]
         if docs.customer:
             domain.append(
