38e751727a974f272ded8ec9bf977fe66878ff6d
Germán Ponce Dominguez <german.ponce@argil.mx>
2024-04-11 17:10:33 -0600

    Actualizaciones

--- l10n_mx_einvoice_manual_currency_exchange_rate/models/account_invoice.py
+++ l10n_mx_einvoice_manual_currency_exchange_rate/models/account_invoice.py
@@ -383,5 +383,39 @@ class account_invoice(models.Model):
             if in_draft_mode:
                 taxes_map_entry['tax_line'].update(taxes_map_entry['tax_line']._get_fields_onchange_balance(force_computation=True))
 
+    def _check_balanced(self):
+        _logger.info("\n############# _check_balanced >>>>>>>>>>>> ")
+        context = self._context
+        _logger.info("\n############# context: %s" % context)
+        ''' Assert the move is fully balanced debit = credit.
+        An error is raised if it's not the case.
+        '''
+        moves = self.filtered(lambda move: move.line_ids)
+        if not moves:
+            return
+
+        # /!\ As this method is called in create / write, we can't make the assumption the computed stored fields
+        # are already done. Then, this query MUST NOT depend of computed stored fields (e.g. balance).
+        # It happens as the ORM makes the create with the 'no_recompute' statement.
+        self.env['account.move.line'].flush(self.env['account.move.line']._fields)
+        self.env['account.move'].flush(['journal_id'])
+        self._cr.execute('''
+            SELECT line.move_id, ROUND(SUM(line.debit - line.credit), currency.decimal_places)
+            FROM account_move_line line
+            JOIN account_move move ON move.id = line.move_id
+            JOIN account_journal journal ON journal.id = move.journal_id
+            JOIN res_company company ON company.id = journal.company_id
+            JOIN res_currency currency ON currency.id = company.currency_id
+            WHERE line.move_id IN %s
+            GROUP BY line.move_id, currency.decimal_places
+            HAVING ROUND(SUM(line.debit - line.credit), currency.decimal_places) != 0.0;
+        ''', [tuple(self.ids)])
+
+        query_res = self._cr.fetchall()
+        if query_res:
+            ids = [res[0] for res in query_res]
+            sums = [res[1] for res in query_res]
+            raise UserError(_("Cannot create unbalanced journal entry. Ids: %s\nDifferences debit - credit: %s") % (ids, sums))
+
 
 # # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
